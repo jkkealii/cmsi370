62810c8b838c0130307adba185f0b64997b9d4c6
Josh <jkkealii@gmail.com>
2015-10-28 21:05:32 -0700

    slowly working

--- front-end/frendjsass.js
+++ front-end/frendjsass.js
@@ -1,15 +1,43 @@
 Twitch.init({clientId: 'n49oe0lfq6jdohv7lidhdnfqpdw0z8u'}, function(error, status) {
     // the sdk is now loaded
+    if (error) {
+        // error encountered while loading
+        console.log(error);
+    }
+    if (status.authenticated) {
+    // user is currently logged in
+    $('.twitch-connect').hide()
+    }
+    if (status.authenticated) {
+      // we're logged in :)
+      $('.status input').val('Logged in! Allowed scope: ' + status.scope);
+      // Show the data for logged-in users
+      $('.authenticated').removeClass('hidden');
+    } else {
+      $('.status input').val('Not Logged in! Better connect with Twitch!');
+      // Show the twitch connect button
+      $('.authenticate').removeClass('hidden');
+    }
 });
 
 $('.twitch-connect').click(function() {
     Twitch.login({
         scope: ['user_read', 'channel_read']
     });
+    
+$('#logout button').click(function() {
+        Twitch.logout();
 })
 
-if (status.authenticated) {
-    // Already logged in, hide button
-    $('.twitch-connect').hide()
-}
+$('#get-name button').click(function() {
+    Twitch.api({method: 'user'}, function(error, user) {
+        $('#get-name input').val(user.display_name);
+    });
+})
+
+$('#get-stream-key button').click(function() {
+    Twitch.api({method: 'channel'}, function(error, channel) {
+        $('#get-stream-key input').val(channel.stream_key);
+    });
+})
 
